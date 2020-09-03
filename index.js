34f3b2a3d29f966c774dce6ec1755140c940a011
paul_vonhagen <pvonhagen26@gmail.com>
2020-09-03 00:52:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(source, fn){
+    let newArray = []
+    for (let i=0; i<source.length; i++){
+        let index = source[i]
+        newArray.push(fn(index))
+    }
+    return newArray;
+}
+
+function reduce(source, fn, start){
+    let r = (!!start) ? start : source[0]
+    let i = (!!start) ? 0 : 1
+    for (; i<source.length; i++){
+        r = fn(source[i], r)
+    }
+    return r;
+}
